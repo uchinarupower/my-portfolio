f5958909e61a13fc3e940e03ab6df654b086556b
uchinarupower <uchikawa.k.th@gmail.com>
2023-03-16 21:49:51 +0900

    change timeline

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,40 +1,67 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+// Event.js
+
+import React, { useState } from 'react';
+import './Event.css';
+
+const events = [
+  {
+    date: 'January 1, 2023',
+    title: 'New Year\'s Day',
+    description: 'The first day of the year in the Gregorian calendar.',
+    details: 'New Year\'s Day is a national holiday celebrated on January 1st. It marks the first day of the year in the Gregorian calendar, and is a time for new beginnings and resolutions.'
+  },
+  {
+    date: 'March 11, 2023',
+    title: 'Example Event',
+    description: 'This is an example event for the timeline.',
+    details: 'This is a sample event for the timeline. It has no real significance, but serves as an example of how to structure the data for the timeline.'
+  },
+  {
+    date: 'June 21, 2023',
+    title: 'Summer Solstice',
+    description: 'The longest day of the year in the Northern Hemisphere.',
+    details: 'The summer solstice is the longest day of the year in the Northern Hemisphere. It occurs around June 21st, and marks the beginning of summer.'
+  },
+  {
+    date: 'October 31, 2023',
+    title: 'Halloween',
+    description: 'A holiday celebrated on October 31st in many countries around the world.',
+    details: 'Halloween is a holiday celebrated on October 31st in many countries around the world. It is often associated with costumes, trick-or-treating, and spooky decorations.'
+  }
+];
 
 const Event = () => {
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  const handleEventClick = (event) => {
+    setSelectedEvent(event);
+  }
+
+  const handleClose = () => {
+    setSelectedEvent(null);
+  }
+
   return (
-    <Container>
-      <Row>
-        <Col md={3} className="text-center">
-          <p>2020</p>
-        </Col>
-        <Col md={9}>
-          <h2>Event Title</h2>
-          <p>Event description goes here...</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={{ span: 9, offset: 3 }}>
-          <h2>Another Event Title</h2>
-          <p>Another event description goes here...</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={3} className="text-center">
-          <p>2019</p>
-        </Col>
-        <Col md={9}>
-          <h2>Event Title</h2>
-          <p>Event description goes here...</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={{ span: 9, offset: 3 }}>
-          <h2>Another Event Title</h2>
-          <p>Another event description goes here...</p>
-        </Col>
-      </Row>
-    </Container>
+    <div className="container">
+      <h1>Event Timeline</h1>
+      <div className="timeline">
+        {events.map((event) => (
+          <div key={event.date} className="event" onClick={() => handleEventClick(event)}>
+            <div className="date">{event.date}</div>
+            <div className="title">{event.title}</div>
+          </div>
+        ))}
+      </div>
+      {selectedEvent && (
+        <div className="modal" onClick={handleClose}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <h2>{selectedEvent.title}</h2>
+            <p>{selectedEvent.description}</p>
+            <p>{selectedEvent.details}</p>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
